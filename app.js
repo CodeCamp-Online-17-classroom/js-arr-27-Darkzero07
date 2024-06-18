63a03486eb9d3c129cac9d550842e9145b2fdca3
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-18 22:54:34 +0700

    js lab 113

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// write code here
+const tasks = [
+  { id: 1, name: "Fishing" },
+  { id: 2, name: "Shopping" },
+  { id: 3, name: "Swimming" },
+];
+
+// updateTask(2, 'Travelling');
+// console.log(tasks);
+// [
+//   { id: 1, name: 'Fishing' },
+//   { id: 2, name: 'Travelling' },
+//   { id: 3, name: 'Swimming' }
+// ]
+
+function updateTask(id, newName) {
+  const element = tasks.find((element) => element.id === id);
+  if (element) {
+    element.name = newName;
+  }
+}
+
+updateTask(2, "Travelling");
+console.log(tasks);
